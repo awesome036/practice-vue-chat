06dfea80d172c520eda12ec2087a8d7df7d3d496
awesome036 <hillzen.asia@gmail.com>
2021-05-25 21:59:28 +0900

    [lol] star-wars text

--- src/frontend/src/components/Chat.vue
+++ src/frontend/src/components/Chat.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    <h1>チャット</h1>
-    <ul>
-      <li v-for="(msg, index) in messages" :key="index">{{ msg.message }}</li>
-    </ul>
     <div>
-      <input
-        type="text"
+      <v-text-field
         @keyup.enter="sendMessage(message)"
         v-model="message"
+        dark
+        placeholder="Enter your history."
+        height="48"
       />
     </div>
+    <ul>
+      <li class="star-wars-text move-message" v-for="(msg, index) in messages" :key="index">{{ msg.message }}</li>
+    </ul>
   </div>
 </template>
 
@@ -23,7 +24,7 @@ export default {
     return {
       messages: [],
       message: "",
-      socket: io("http://192.168.56.107:3000", {
+      socket: io("http://localhost:3000", {
         withCredentials: true,
         extraHeaders: {
           "my-custom-header": "abcd",
@@ -44,3 +45,35 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.star-wars-text {
+  display: inline-block;
+  visibility: hidden;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  list-style: none;
+  color: yellow;
+  line-height: 0.9;
+  font-weight: bold;
+  text-align: center;
+  transform: perspective(20px) rotateX(60deg);
+}
+
+.move-message {
+  animation: move-bottom-to-top 20s linear;
+}
+@keyframes move-bottom-to-top {
+  0% {
+    visibility: visible;
+    font-size: 6rem;
+    transform: translateY(200px) perspective(80px) rotateX(30deg);
+  }
+  100% {
+    visibility: hidden;
+    font-size: 0rem;
+    transform: translateY(-200px) perspective(80px) rotateX(30deg);
+  }
+}
+</style>
